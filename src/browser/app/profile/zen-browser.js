b73e15a172d6df72121921a9bc0a42bf6d91b214
mauro-balades <mauro.balades@tutanota.com>
2024-03-29 18:53:01 +0100

    Edited and fixed UI

---
+++ src/browser/app/profile/zen-browser.js
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+pref("browser.tabs.cardPreview.enabled", true);
+pref("browser.tabs.cardPreview.delayMs", 500);
+
+pref("browser.urlbar.suggest.calculator", true);
+
+#ifdef MOZ_UPDATE_CHANNEL
+pref("devtools.debugger.prompt-connection", false);
+#endif

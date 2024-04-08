ffbaa42e6c7c94b43f65cc4df89f72328d8b3807
Mauro Balades <mauro.balades@tutanota.com>
2024-04-08 23:03:34 +0200

    Init script compiles the new tab now

--- init-browser.py
+++ init-browser.py
@@ -4,8 +4,13 @@ import os
 def initialize_avatars():
   os.system("cd src/browser/base/content/zen-avatars && python3 fetch-all-avatars.py")
 
+def recompile_new_tab():
+  os.system("(cd ./engine/browser/components/newtab && ../../../mach npm install && ../../../mach npm install meow@9.0.0)")
+  os.system("cd ./engine && ./mach npm run bundle --prefix=browser/components/newtab")
+
 def main():
   initialize_avatars()
+  recompile_new_tab()
 
 if __name__ == "__main__":
   main()

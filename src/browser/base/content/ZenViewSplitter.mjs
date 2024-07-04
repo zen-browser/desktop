732d1e7a37eafeeb5097c75773cd4785ab3b707e
Mauro Balades <mauro.balades@tutanota.com>
2024-07-04 23:43:01 +0200

    chore: Update CSS styles for Zen Browser sidebar and tabpanels

--- src/browser/base/content/ZenViewSplitter.mjs
+++ src/browser/base/content/ZenViewSplitter.mjs
@@ -8,7 +8,7 @@ var gZenViewSplitter = {
    *      tab2,
    *      tab3,
    *     ],
-   *    gridType: "vsplit" | "hsplit" | "grid",
+   *    gridType: "vsep" | "hsep" | "grid",
    *   }
    * ]
    */
@@ -45,10 +45,14 @@ var gZenViewSplitter = {
     dataTab.splice(dataTab.indexOf(tab), 1);
     tab._zenSplitted = false;
     tab.linkedBrowser.zenModeActive = false;
-    tab.linkedBrowser.docShellIsActive = false;
     let container = tab.linkedBrowser.closest(".browserSidebarContainer");
     container.removeAttribute("zen-split");
-    container.style.display = "none";         
+    if (!event.forUnsplit) {
+      tab.linkedBrowser.docShellIsActive = false;
+      container.style.display = "none";         
+    } else {
+      container.style.gridArea = "1 / 1";
+    }
     if (dataTab.length < 2) {
       this._data.splice(index, 1);
       if (this.currentView == index) {
@@ -61,7 +65,8 @@ var gZenViewSplitter = {
         for (const tab of dataTab) {
           let container = tab.linkedBrowser.closest(".browserSidebarContainer");
           container.removeAttribute("zen-split");
-          container.style.gridArea = "1 / 1";                 
+          container.style.gridArea = "1 / 1";
+          tab._zenSplitted = false;               
         }     
       }
       return;
@@ -72,6 +77,7 @@ var gZenViewSplitter = {
 
   onLocationChange(browser) {
     let tab = gBrowser.getTabForBrowser(browser);
+    this.updateSplitViewButton(!(tab && tab._zenSplitted));
     if (!tab) {
       return;
     }
@@ -85,6 +91,7 @@ var gZenViewSplitter = {
     }
     this._data.push({
       tabs,
+      gridType: "grid",
     });
     gBrowser.selectedTab = tabs[0];
     this._showSplitView(tabs[0]);
@@ -110,6 +117,7 @@ var gZenViewSplitter = {
       })
     };    
     if (!splitData || (this.currentView >= 0 && !this._data[this.currentView].tabs.includes(tab))) {
+      this.updateSplitViewButton(true);
       if (this.currentView < 0) {
         return;
       }
@@ -176,8 +184,13 @@ var gZenViewSplitter = {
       if (currentRowGridArea[1] != "") {
         this.tabBrowserPanel.style.gridTemplateAreas += `     '${currentRowGridArea[1]}'`;
       }
+    } else if (gridType == "vsep") {
+      this.tabBrowserPanel.style.gridTemplateAreas = `'${splitData.tabs.map((_, i) => `tab${i + 1}`).join(" ")}'`;
+    } else if (gridType == "hsep") {
+      this.tabBrowserPanel.style.gridTemplateAreas = `${splitData.tabs.map((_, i) => `'tab${i + 1}'`).join(" ")}`;
     }
     modifyDecks(splitData.tabs, true);
+    this.updateSplitViewButton(false);
   },
 
   contextSplitTabs() {
@@ -196,6 +209,62 @@ var gZenViewSplitter = {
       }
     }
     return true;
+  },
+
+  // Panel and url button
+
+  updateSplitViewButton(hidden) {
+    let button = document.getElementById("zen-split-views-box");
+    if (hidden) {
+      button.setAttribute("hidden", "true");
+      return;
+    }
+    button.removeAttribute("hidden");
+  },
+
+  get _modifierElement() {
+    if (!this.__modifierElement) {
+      let wrapper = document.getElementById("template-zen-split-view-modifier");
+      const panel = wrapper.content.firstElementChild;
+      wrapper.replaceWith(wrapper.content);
+      this.__modifierElement = panel;
+    }
+    return this.__modifierElement;
+  },
+
+  async openSplitViewPanel(event) {
+    let panel = this._modifierElement;
+    let target = event.target.parentNode;
+    for (const gridType of ["hsep", "vsep", "grid", "unsplit"]) {
+      let selector = panel.querySelector(`.zen-split-view-modifier-preview.${gridType}`);
+      selector.classList.remove("active");
+      if (this.currentView >= 0 && this._data[this.currentView].gridType == gridType) {
+        selector.classList.add("active");
+      }
+      if (this.__hasSetMenuListener) {
+        continue;
+      }
+      selector.addEventListener("click", ((gridType) => {
+        if (gridType === "unsplit") {
+          let tabs = this._data[this.currentView].tabs;
+          for (const tab of tabs) {
+            this.onTabClose({ target: tab, forUnsplit: true });
+          }
+          gBrowser.selectedTab = tabs[0];
+          panel.hidePopup();
+          this.updateSplitViewButton(true);
+          return;
+        }
+        this._data[this.currentView].gridType = gridType;
+        this._showSplitView(gBrowser.selectedTab);
+        panel.hidePopup();
+      }).bind(this, gridType));
+    } 
+    this.__hasSetMenuListener = true;
+    PanelMultiView.openPopup(panel, target, {
+      position: "bottomright topright",
+      triggerEvent: event,
+    }).catch(console.error);
   },
 };
 

e1e910e339861c5652a4459600973d075090dd80
Mauro Balades <mauro.balades@tutanota.com>
2024-07-19 19:54:09 +0200

    chore: Update xvfb-run command in alpha-build.sh script

--- .github/workflows/src/alpha-build.sh
+++ .github/workflows/src/alpha-build.sh
@@ -1,3 +1,10 @@
 
 export ZEN_RELEASE=1
-xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" pnpm build
+# Check if xfvb is installed
+if ! command -v xvfb-run &> /dev/null
+then
+    echo "xvfb-run could not be found, running without it"
+    pnpm build
+else
+  xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" pnpm build
+fi

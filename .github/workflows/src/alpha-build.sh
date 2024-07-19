8c1e1bc97d5587bbd8f5c0f5c20d10262aeb1fb9
Mauro Balades <mauro.balades@tutanota.com>
2024-07-19 22:35:31 +0200

    chore: Update alpha-build.sh script to handle xvfb-run not found, enable LTO only when not cross-compiling, and set LLVM_PROFDATA environment variable

--- .github/workflows/src/alpha-build.sh
+++ .github/workflows/src/alpha-build.sh
@@ -6,6 +6,7 @@ then
   echo "xvfb-run could not be found, running without it"
   pnpm build
 else
+  export LLVM_PROFDATA=$HOME/.mozbuild/clang/bin/llvm-profdata
   export DISPLAY=:2
   xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" pnpm build
 fi

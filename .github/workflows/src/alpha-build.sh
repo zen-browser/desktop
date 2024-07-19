93ba4e0638b08561f154feb9eebd66af1e11fe3a
Mauro Balades <mauro.balades@tutanota.com>
2024-07-19 22:01:55 +0200

    chore: Update alpha-build.sh script to handle xvfb-run not found and enable LTO only when not cross-compiling

--- .github/workflows/src/alpha-build.sh
+++ .github/workflows/src/alpha-build.sh
@@ -3,8 +3,9 @@ export ZEN_RELEASE=1
 # Check if xfvb is installed
 if ! command -v xvfb-run &> /dev/null
 then
-    echo "xvfb-run could not be found, running without it"
-    pnpm build
+  echo "xvfb-run could not be found, running without it"
+  pnpm build
 else
+  export DISPLAY=:2
   xvfb-run -s "-screen 0 1920x1080x24 -nolisten local" pnpm build
 fi

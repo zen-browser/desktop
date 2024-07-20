093a9625a768d02c4a4a96f6fccaaae7dcc3e04b
Mauro Balades <mauro.balades@tutanota.com>
2024-07-20 02:00:37 +0200

    chore: Update alpha-build.sh script to handle xvfb-run not found and enable LTO only when not cross-compiling

--- .github/workflows/src/alpha-build.sh
+++ .github/workflows/src/alpha-build.sh
@@ -1,20 +1,21 @@
+set -x
 
 export ZEN_RELEASE=1
 
-if command -v apt-get &> /dev/null
+if command -v apt &> /dev/null
 then
-  sudo apt-get update
-  sudo apt-get install -y xvfb
+  sudo apt update
+  sudo apt install -y xvfb
 fi
 
 # Check if xfvb is installed
 if ! command -v Xvfb &> /dev/null
 then
-  echo "Xvfb could not be found, running without it"
-  pnpm build
-else
   Xvfb :2 -screen 0 1024x768x24 &
   export LLVM_PROFDATA=$HOME/.mozbuild/clang/bin/llvm-profdata
   export DISPLAY=:2
+  pnpm build
+else
+  echo "Xvfb could not be found, running without it"
+  pnpm build
 fi
-pnpm build

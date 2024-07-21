84f9237c91b57415c6e66dc7e21732c0be0c0e44
Mauro Balades <mauro.balades@tutanota.com>
2024-07-21 09:54:39 +0200

    new workflow implementation

---
+++ .github/workflows/src/disk-setup-and-swap.sh
@@ -0,0 +1,43 @@
+
+echo Before:
+free -h
+df -h
+
+echo
+echo
+
+sudo swapoff /mnt/swapfile
+sudo rm /mnt/swapfile
+sudo fallocate -l 10G /mnt/swapfile
+sudo chmod 600 /mnt/swapfile
+sudo mkswap /mnt/swapfile
+sudo swapon /mnt/swapfile
+sudo apt remove -y '^dotnet-.*' '^llvm-.*' '^php.*' '^mongodb-.*' '^mysql-.*' clang azure-cli google-cloud-sdk google-chrome-stable microsoft-edge firefox powershell mono-devel libgl1-mesa-dri npm acl aria2 autoconf automake binutils bison brotli bzip2 coreutils 
+sudo apt autoremove -y
+sudo apt clean
+sudo rm -rf  ./git
+sudo rm -rf /home/linuxbrew
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/graalvm
+sudo rm -rf /usr/local/share/powershell
+sudo rm -rf /usr/local/share/chromium
+sudo rm -rf /usr/local/lib/node_modules
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/share/boost
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf /etc/apache2
+sudo rm -rf /etc/nginx
+sudo rm -rf /usr/local/share/chrome_driver
+sudo rm -rf /usr/local/share/edge_driver
+sudo rm -rf /usr/local/share/gecko_driver
+sudo rm -rf /usr/share/java
+sudo rm -rf /usr/share/miniconda
+sudo rm -rf /usr/local/share/vcpkg
+
+echo
+echo
+
+echo After:
+free -h
+df -h

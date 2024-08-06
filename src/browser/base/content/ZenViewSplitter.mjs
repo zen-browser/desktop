a513d95aad7f3d3c5920f1f25224def1b4e33320
Mauro Balades <mauro.balades@tutanota.com>
2024-08-06 12:04:59 +0200

    chore: Add lazy loading for UI elements in split view

--- src/browser/base/content/ZenViewSplitter.mjs
+++ src/browser/base/content/ZenViewSplitter.mjs
@@ -1,19 +1,78 @@
 
 var gZenViewSplitter = {
   init() {
+    this.initializeUI();
     console.log(gZenSplitViewsBase)
   },
 
-  onLocationChange(browser) {
+  initializeUI() {
+    this.initializeUpdateContextMenuItems();
+    this.initializeTabContextMenu();
   },
 
-  contextCanSplitTabs() {
+  initializeTabContextMenu() {
+    const fragment = window.MozXULElement.parseXULToFragment(`
+      <menuseparator/>
+      <menuitem id="context_zenSplitTabs"
+                data-lazy-l10n-id="tab-zen-split-tabs"
+                oncommand="gZenViewSplitter.contextSplitTabs();"/>
+    `);
+    document.getElementById("tabContextMenu").appendChild(fragment);
+  },
+
+  /**
+   * context menu item display update
+   */
+  initializeUpdateContextMenuItems() {
+    const contentAreaContextMenu = document.getElementById("tabContextMenu");
+    const tabCountInfo = JSON.stringify({
+      tabCount:
+        (window.gContextMenu?.contextTab.multiselected &&
+          window.gBrowser.multiSelectedTabsCount) ||
+        1,
+    });
+
+    contentAreaContextMenu.addEventListener("popupshowing", () => {
+      document
+        .getElementById("context_zenSplitTabs")
+        .setAttribute("data-l10n-args", tabCountInfo);
+      document.getElementById("context_zenSplitTabs").disabled =
+        !this.contextCanSplitTabs();
+    });
+  },
+
+  onLocationChange(browser) {
+    gZenSplitViewsBase.onLocationChange(browser);
   },
 
   openSplitViewPanel(event) {
   },
 
+  contextCanSplitTabs() {
+    let tabs = window.gBrowser.selectedTabs;
+    if (tabs.length < 2) {
+      return false;
+    }
+    // Check if there are 2 tabs in different groups
+    // Or if all the selected tabs are in the same group
+    let group = gZenSplitViewsBase.getTabView(tabs[0]);
+    for (let i = 1; i < tabs.length; i++) {
+      // Check if they are not in the same group, but we do allow
+      // if they are ungrouped
+      let tabGroup = gZenSplitViewsBase.getTabView(tabs[i]);
+      if (tabGroup === -1) {
+        continue;
+      }
+      if (group !== tabGroup) {
+        return false;
+      }
+    }
+    return true;
+  },
+
   contextSplitTabs() {
+    let selectedTabs = gBrowser.selectedTabs;
+    
   },
 };
 

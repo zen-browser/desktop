98b2c582f242f9d717893824d1be6662a79c644a
HarryHeres <personal@janheres.eu>
2024-08-31 23:53:37 +0200

    Testing out Brew casks

---
+++ Cask/zen-browser.rb
@@ -0,0 +1,12 @@
+cask "zen-browser" do
+  version ""
+  sha256 ""
+
+  url "https://github.com/zen-browser/desktop/releases/download/#{version}/-zen.macos-aarch64.dmg"
+  name "Zen Browser"
+  desc "Beautifully designed, privacy-focused browser packed with awesome jfeatures"
+  homepage "https://github.com/zen-browser/desktop"
+
+  app "Zen Browser.app"
+end
+

cf9b266bcaddd658a3e6b41f9560d0d6fc6fe410
HarryHeres <personal@janheres.eu>
2024-09-01 19:10:25 +0200

    Adding Homebrew compatibility

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,10 +1,7 @@
 cask("zen-browser") do
   arch(arm: "aarch64", intel: "x64")
   version("1.0.0-a.34")
-  sha256(
-    arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
-    intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6"
-  )
+  sha256(arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747", intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6")
 
   url("https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg")
   name("Zen Browser")
@@ -16,12 +13,10 @@ cask("zen-browser") do
     regex(/[0-9].*-a.[0-9]*/i)
   end
 
-  zap = [
-    "$HOME/Library/Preferences/org.mozilla.com.zen.browser.plist",
-    "$HOME/Library/Saved Application State/org.mozilla.com.zen.browser.savedState"
+  zap trash: [
+    "~/Library/Preferences/org.mozilla.com.zen.browser.plist",
+    "~/Library/Saved Application State/org.mozilla.com.zen.browser.savedState"
   ]
-
-  depends_on = []
 
   app("Zen Browser.app")
 

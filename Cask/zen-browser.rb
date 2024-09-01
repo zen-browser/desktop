dfa316831ff3536ccf892b4e9696e4b8d029033f
HarryHeres <personal@janheres.eu>
2024-09-01 17:35:01 +0200

    Testing out Homebrew cask

--- Cask/zen-browser.rb
+++ Cask/zen-browser.rb
@@ -1,11 +1,21 @@
 cask "zen-browser" do
-  version ""
-  sha256 ""
+  arch arm: "aarch64", intel: "x64"
+  version "1.0.0-a.34"
+  sha256 arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace258074",
+    intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6"
 
-  url "https://github.com/zen-browser/desktop/releases/download/#{version}/-zen.macos-aarch64.dmg"
+  url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg"
   name "Zen Browser"
   desc "Beautifully designed, privacy-focused browser packed with awesome jfeatures"
-  homepage "https://github.com/zen-browser/desktop"
+  homepage "https://zen-browser.app/"
+
+  livecheck do
+    url :stable
+    regex(/[0-9].*-a.[0-9]*/i)
+  end
+
+  zap = []
+  depends_on = [] 
 
   app "Zen Browser.app"
 end

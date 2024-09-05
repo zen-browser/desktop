f7612bd61a2d60352cb55bc7344e2a0054c82dfd
AltCode <12354672+AlternateRT@users.noreply.github.com>
2024-09-05 19:54:35 +0200

    Make cask style conformant to Homebrew guidelines

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,19 +1,19 @@
-cask("zen-browser") do
-  arch(arm: "aarch64", intel: "x64")
+cask "zen-browser" do
+  arch arm: "aarch64", intel: "x64"
 
-  version("1.0.0-a.34")
-  sha256(arm:   "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
-         intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6")
+  version "1.0.0-a.34"
+  sha256 arm:   "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
+         intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6"
 
-  url("https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg")
-  name("Zen Browser")
-  desc("Beautifully designed, privacy-focused browser packed with awesome features")
-  homepage("https://zen-browser.app/")
+  url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg"
+  name "Zen Browser"
+  desc "Beautifully designed, privacy-focused browser packed with awesome features"
+  homepage "https://zen-browser.app/"
 
-  app("Zen Browser.app")
+  app "Zen Browser.app"
 
   postflight do
-    system("xattr -c '/Applications/Zen Browser.app/'")
+    system "xattr -c '/Applications/Zen Browser.app/'"
   end
 
   zap trash: [

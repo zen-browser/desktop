700d1e935774da8de6378508504c60bc661a6413
AltCode <12354672+AlternateRT@users.noreply.github.com>
2024-09-05 19:55:27 +0200

    Add url verification to cask

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -5,7 +5,8 @@ cask "zen-browser" do
   sha256 arm:   "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
          intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6"
 
-  url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg"
+  url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg",
+      verified: "github.com/zen-browser/desktop/"
   name "Zen Browser"
   desc "Beautifully designed, privacy-focused browser packed with awesome features"
   homepage "https://zen-browser.app/"

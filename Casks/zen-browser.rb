f1134c50c10141684acf202c4948ec870d27a75a
AltCode <12354672+AlternateRT@users.noreply.github.com>
2024-09-05 19:52:23 +0200

    Fixed stanza order in cask file
    Fixed the stanza order by running brew style --fix zen-browser

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,21 +1,23 @@
 cask("zen-browser") do
   arch(arm: "aarch64", intel: "x64")
+
   version("1.0.0-a.34")
-  sha256(arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747", intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6")
+  sha256(arm:   "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
+         intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6")
 
   url("https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg")
   name("Zen Browser")
   desc("Beautifully designed, privacy-focused browser packed with awesome features")
   homepage("https://zen-browser.app/")
 
-  zap trash: [
-    "~/Library/Preferences/org.mozilla.com.zen.browser.plist",
-    "~/Library/Saved Application State/org.mozilla.com.zen.browser.savedState"
-  ]
-
   app("Zen Browser.app")
 
   postflight do
     system("xattr -c '/Applications/Zen Browser.app/'")
   end
+
+  zap trash: [
+    "~/Library/Preferences/org.mozilla.com.zen.browser.plist",
+    "~/Library/Saved Application State/org.mozilla.com.zen.browser.savedState",
+  ]
 end

7bfe75b8596c19eb6fd58d9d8ad69b993d494966
mauro-balades <mauro.balades@tutanota.com>
2024-09-07 21:09:34 +0200

    Refactor l10n: Update subproject commit reference
    Copy the rest of the l10n directories to their respective locations in copy-language-pack.sh

--- scripts/copy-language-pack.sh
+++ scripts/copy-language-pack.sh
@@ -9,7 +9,8 @@ copy_browser_locales() {
     return
   fi
   rm -rf $browser_locales/$langId/
-  rsync -av --exclude=.git ./l10n/$langId/ $browser_locales/$langId/
+  # TODO: Copy the rest of the l10n directories to their respective locations
+  rsync -av --exclude=.git ./l10n/$langId/browser/ $browser_locales/$langId/
 }
 
 LANG=$1

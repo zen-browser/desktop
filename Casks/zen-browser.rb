22ffac163dc3e8169e93ee2cd265f7a43a45e0bf
mauro 🤙 <91018726+mauro-balades@users.noreply.github.com>
2024-09-10 10:17:00 +0200

    Update zen-browser.rb

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,7 +1,7 @@
 cask "zen-browser" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.0.0-a.37"
+  version "1.0.0-a.39"
   sha256 arm:   "0cf18406f61225c53ca20391b37dcdcbfd3dd80bf758639b56962e38085e529d",
          intel: "4de8907641989639ee159681cd131a22a20fcf414373543a62ee0f9df2739845"
 

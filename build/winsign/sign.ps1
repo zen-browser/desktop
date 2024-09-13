016d8eb8064db90289de3ef85075c5e879dec5a8
Mauro Baladés <mauro.balades@tutanota.com>
2024-09-13 10:13:11 -0700

    Refactor windows-alpha-build.yml to update artifact path

--- build/winsign/sign.ps1
+++ build/winsign/sign.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = "Stop"
 
 echo "Preparing environment"
 mkdir windsign-temp -ErrorAction SilentlyContinue
-mkdir engine\obj-x86_64-pc-windows-msvc\dist -ErrorAction SilentlyContinue
+mkdir engine\obj-x86_64-pc-windows-msvc\ -ErrorAction SilentlyContinue
 
 pnpm surfer ci --brand alpha
 
@@ -16,15 +16,15 @@ gh run download $RunID --pattern "windows-x64-obj-*" --dir windsign-temp
 
 function SignAndPackage($name) {
     echo "Executing on $name"
-    rmdir engine\obj-x86_64-pc-windows-msvc\dist\ -Recurse -ErrorAction SilentlyContinue
-    mv windsign-temp\windows-x64-obj-$name engine\obj-x86_64-pc-windows-msvc\dist
+    rmdir engine\obj-x86_64-pc-windows-msvc\ -Recurse -ErrorAction SilentlyContinue
+    mv windsign-temp\windows-x64-obj-$name engine\obj-x86_64-pc-windows-msvc\
     echo "Signing $name"
     # Find all executables and dlls and sign them
-    Get-ChildItem engine\obj-x86_64-pc-windows-msvc\dist -Recurse -Filter *.exe | % {
+    Get-ChildItem engine\obj-x86_64-pc-windows-msvc\ -Recurse -Filter *.exe | % {
         echo "Signing $_"
         signtool.exe sign /n "$SignIdentity" /t http://time.certum.pl/ /fd sha1 /v $_.FullName
     }
-    Get-ChildItem engine\obj-x86_64-pc-windows-msvc\dist -Recurse -Filter *.dll | % {
+    Get-ChildItem engine\obj-x86_64-pc-windows-msvc\ -Recurse -Filter *.dll | % {
         echo "Signing $_"
         signtool.exe sign /n "$SignIdentity" /t http://time.certum.pl/ /fd sha1 /v $_.FullName
     }

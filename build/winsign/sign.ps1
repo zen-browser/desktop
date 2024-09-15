473776c0f7ec2ac10b81375a68974007836d47ab
Mauro Baladés <mauro.balades@tutanota.com>
2024-09-15 09:59:04 -0700

    Refactor build/winsign/sign.ps1 to download windows-x64-obj-generic

--- build/winsign/sign.ps1
+++ build/winsign/sign.ps1
@@ -1,25 +1,25 @@
 param(
-    [string][Parameter(Mandatory=$true)]$SignIdentity,
-    [string][Parameter(Mandatory=$true)]$RunID
+    [string][Parameter(Mandatory=$true)]$SignIdentity
 )
 
 $ErrorActionPreference = "Stop"
 
 echo "Preparing environment"
-rmdir windsign-temp -Recurse -ErrorAction SilentlyContinue
 mkdir windsign-temp -ErrorAction SilentlyContinue
+
+echo "Please UNZIP the generic and specific artifacts into windsign-temp"
+echo "With the following filenames:"
+echo "  - $pwd\windsign-temp\windows-x64-obj-specific"
+echo "  - $pwd\windsign-temp\windows-x64-obj-generic"
+Read-Host "Press Enter to continue when ready"
 mkdir engine\obj-x86_64-pc-windows-msvc\ -ErrorAction SilentlyContinue
 
 pnpm surfer ci --brand alpha
 
-echo "Downloading from runner with ID $RunID"
-gh run download $RunID --name "windows-x64-obj-specific" --dir windsign-temp\windows-x64-obj-specific
-gh run download $RunID --name "windows-x64-obj-generic" --dir windsign-temp\windows-x64-obj-generic
-
 function SignAndPackage($name) {
     echo "Executing on $name"
     rmdir engine\obj-x86_64-pc-windows-msvc\ -Recurse -ErrorAction SilentlyContinue
-    mv windsign-temp\windows-x64-obj-$name engine\obj-x86_64-pc-windows-msvc\
+    cp windsign-temp\windows-x64-obj-$name engine\obj-x86_64-pc-windows-msvc\ -Recurse
     echo "Signing $name"
     # Collect all .exe and .dll files into a list
     $files = Get-ChildItem engine\obj-x86_64-pc-windows-msvc\ -Recurse -Include *.exe
@@ -27,13 +27,43 @@ function SignAndPackage($name) {
     signtool.exe sign /n "$SignIdentity" /t http://time.certum.pl/ /fd sha1 /v $files
     echo "Packaging $name"
     $env:SURFER_SIGNING_MODE="sign"
-    $env:MAR="$PWD\build\winsign\mar.exe"
+    $env:MAR="$PWD\\build\\winsign\\mar.exe"
+
+    rm .\dist -Recurse -ErrorAction SilentlyContinue
     pnpm surfer package
 
-    echo "Taring $name"
-    tar -czf windows-x64-signed-$name.tar.gz dist\
+    # In the release script, we do the following:
+    #  tar -xvf .github/workflows/object/windows-x64-signed-generic.tar.gz -C windows-x64-signed-generic
+    # We need to create a tar with the same structure and no top-level directory
+    # Inside, we need:
+    #  - update_manifest/*
+    #  - windows.mar or windows-generic.mar
+    #  - zen.installer.exe or zen.installer-generic.exe
+    #  - zen.win-generic.zip or zen.win-specific.zip
+    echo "Creating tar for $name"
+    rm .\windsign-temp\windows-x64-signed-$name -Recurse -ErrorAction SilentlyContinue
+    mkdir windsign-temp\windows-x64-signed-$name
+    
+    # Move the MAR, add the `-generic` suffix if needed
+    if ($name -eq "generic") {
+        mv .\dist\output.mar windsign-temp\windows-x64-signed-$name\windows-generic.mar
+    } else {
+        mv .\dist\output.mar windsign-temp\windows-x64-signed-$name\windows.mar
+    }
 
-    Move-Item windows-x64-signed-$name.tar.gz .github\workflows\object\windows-x64-signed-$name.tar.gz
+    # Move the installer
+    if ($name -eq "generic") {
+        mv .\dist\zen.installer.exe windsign-temp\windows-x64-signed-$name\zen.installer-generic.exe
+    } else {
+        mv .\dist\zen.installer.exe windsign-temp\windows-x64-signed-$name\zen.installer.exe
+    }
+
+    # Move the zip
+    mv .\dist\zen.win64.zip windsign-temp\windows-x64-signed-$name\zen.win-$name.zip
+
+    # Move the manifest
+    mv .\dist\update\* windsign-temp\windows-x64-signed-$name\
+    tar -cvf .\.github\workflows\object\windows-x64-signed-$name.tar -C .\windsign-temp\windows-x64-signed-$name .
 }
 
 SignAndPackage specific
@@ -42,6 +72,7 @@ SignAndPackage generic
 # Cleaning up
 
 echo "All done!"
+echo "All the artifacts (Generic and Specific) are signed and packaged, get a rest now!"
 Read-Host "Press Enter to continue"
 
 echo "Cleaning up" 

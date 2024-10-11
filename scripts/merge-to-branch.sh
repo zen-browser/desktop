2b68cc481ef86d196cb5baa19fde0f5df33dead7
mr. M <mauro.balades@tutanota.com>
2024-10-11 19:13:55 +0200

    Refactor merge-to-branch script to handle "zen-stable" branch and merge to "twilight" branch if specified

--- scripts/merge-to-branch.sh
+++ scripts/merge-to-branch.sh
@@ -3,6 +3,10 @@ set -ex
 branch=zen-"$1"
 default_branch="central"
 
+if [ $branch = "zen-stable" ]; then
+  $(dirname $0)/merge-to-branch.sh twilight
+fi
+
 git checkout "$branch"
 git merge "$default_branch"
 git push origin "$branch"

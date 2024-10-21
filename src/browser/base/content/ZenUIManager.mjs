45feac7c953535567ff89fcba1a7067ad3427931
ricky <richardwoesiii@gmail.com>
2024-10-21 12:05:02 -0400

    Refactor ZenUIManager and gZenVerticalTabsManager
    This commit refactors the ZenUIManager and gZenVerticalTabsManager modules. It introduces the use of const for variables and improves code readability. The changes include:
    - Replacing the use of var with const in gZenUIManager and gZenVerticalTabsManager.
    - Updating the openAndChangeToTab function in ZenUIManager to use const for the tab variable.
    - Updating the initRightSideOrderContextMenu function in gZenVerticalTabsManager to use const for the tabs variable.

--- src/browser/base/content/ZenUIManager.mjs
+++ src/browser/base/content/ZenUIManager.mjs
@@ -8,11 +8,11 @@ var gZenUIManager = {
 
   openAndChangeToTab(url, options) {
     if (window.ownerGlobal.parent) {
-      let tab = window.ownerGlobal.parent.gBrowser.addTrustedTab(url, options);
+      const tab = window.ownerGlobal.parent.gBrowser.addTrustedTab(url, options);
       window.ownerGlobal.parent.gBrowser.selectedTab = tab;
       return tab;
     }
-    let tab = window.gBrowser.addTrustedTab(url, options);
+    const tab = window.gBrowser.addTrustedTab(url, options);
     window.gBrowser.selectedTab = tab;
     return tab;
   },
@@ -85,7 +85,7 @@ var gZenVerticalTabsManager = {
     this._updateEvent();
     this.initRightSideOrderContextMenu();
 
-    let tabs = document.getElementById('tabbrowser-tabs');
+    const tabs = document.getElementById('tabbrowser-tabs');
 
     XPCOMUtils.defineLazyPreferenceGetter(this, 'canOpenTabOnMiddleClick', 'zen.tabs.newtab-on-middle-click', true);
 

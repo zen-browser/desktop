4a392a8a4a79ce7eec450f343fd52e7ea24ae64e
Slowlife01 <slowlife1165@gmail.com>
2025-03-02 19:21:32 +0700

    Set empty tab state hidden when not visible

--- src/browser/base/zen-components/ZenWorkspaces.mjs
+++ src/browser/base/zen-components/ZenWorkspaces.mjs
@@ -145,6 +145,12 @@ var ZenWorkspaces = new (class extends ZenMultiWindowFeature {
 
   _initializeEmptyTab() {
     this._emptyTab = gBrowser.addTrustedTab('about:blank', { inBackground: true, userContextId: 0, _forZenEmptyTab: true });
+
+    gBrowser.tabContainer.addEventListener("TabSelect", () => {
+      if (this._emptyTab && gBrowser.selectedTab !== this._emptyTab) {
+        gBrowser.hideTab(this._emptyTab);
+      }
+    });
   }
 
   registerPinnedResizeObserver() {

5d3e6d2607b673c520744096a8c598f291ef0fe5
Stefan Tanuwijaya <rstanuwijaya@connect.ust.hk>
2025-05-22 23:25:28 +0800

    feat: Add new prefs to enable tab search

--- src/browser/components/preferences/zen-settings.js
+++ src/browser/components/preferences/zen-settings.js
@@ -1170,6 +1170,11 @@ Preferences.addAll([
     default: true,
   },
   {
+    id: 'zen.tabsearch.enabled',
+    type: 'bool',
+    default: true,
+  },
+  {
     id: 'zen.tabsearch.auto-select-result',
     type: 'bool',
     default: false,

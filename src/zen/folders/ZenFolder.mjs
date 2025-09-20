5f5acb2d2a609088d60fe39fe932765966c2facb
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2025-09-20 15:54:21 +0200

    Revert "chore: Rollback from new folder animation until release (#10451)"
    This reverts commit 59dcf4c037d478b21eb5f61459ee90256d155e45.

--- src/zen/folders/ZenFolder.mjs
+++ src/zen/folders/ZenFolder.mjs
@@ -144,6 +144,10 @@
       return activeGroups;
     }
 
+    get childActiveGroups() {
+      return Array.from(this.querySelectorAll('zen-folder[has-active]'));
+    }
+
     rename() {
       if (!document.documentElement.hasAttribute('zen-sidebar-expanded')) {
         return;
@@ -267,9 +271,10 @@
     }
 
     async #unloadAllActiveTabs(event, noClose = false) {
-      for (const tab of this.tabs) {
-        await gZenPinnedTabManager._onCloseTabShortcut(event, tab, { noClose });
-      }
+      await gZenPinnedTabManager._onCloseTabShortcut(event, this.tabs, {
+        noClose,
+        folderToUnload: this,
+      });
       this.activeTabs = [];
     }
 

4e96f579f6ccdb964b656d5c336d083a696a44c4
mr. m <mr.m@tuta.com>
2025-12-11 13:05:43 +0100

    chore: Implement settings dump schema to update firefox remote services data, b=no-bug, c=configs, scripts

---
+++ scripts/update_service_dumps.py
@@ -0,0 +1,67 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import json
+
+DUMPS_FOLDER = os.path.join(
+    'configs', 'dumps'
+)
+ENGINE_DUMPS_FOLDER = os.path.join(
+    'engine', 'services', 'settings', 'dumps', 'main'
+)
+
+
+def merge_dumps(original, updates):
+  """Merges two dumps by removing entries specified in updates from original."""
+  remove_ids = updates.get('remove', {"identifiers": []}).get('identifiers', [])
+
+  # Filter out entries in original that are in remove_ids.
+  #  We may find example-* patterns, so we need to handle that as well.
+  merged_data = [
+      entry for entry in original.get('data', [])
+      if not any(
+          entry.get('identifier', '') == rid or
+          (rid.endswith('*') and entry.get('identifier', '').startswith(rid[:-1]))
+          for rid in remove_ids
+      )
+  ]
+
+  return {
+      'data': merged_data,
+      **{k: v for k, v in original.items() if k != 'data'},
+  }
+
+
+def main():
+  print("Updating service dumps...")
+  for filename in os.listdir(DUMPS_FOLDER):
+    if filename.endswith('.json'):
+      #  parse json with comments
+      with open(os.path.join(DUMPS_FOLDER, filename), 'r', encoding='utf-8') as f:
+        content = f.read()
+        content = '\n'.join(
+            line for line in content.split('\n') if not line.lstrip(' ').startswith('//')
+        )
+        data = json.loads(content)
+      original_path = os.path.join(ENGINE_DUMPS_FOLDER, filename)
+      if os.path.exists(original_path):
+        with open(original_path, 'r') as f:
+          original_content = f.read()
+          original_content = '\n'.join(
+              line for line in original_content.split('\n') if not line.lstrip(' ').startswith('//')
+          )
+          original_data = json.loads(original_content)
+        merged_data = merge_dumps(original_data, data)
+        with open(original_path, 'w', encoding='utf-8') as f:
+          #  do not write special symbols as \uXXXX
+          json.dump(merged_data, f, indent=2, ensure_ascii=False)
+        print(f"Updated dump: {filename}")
+      else:
+        print(f"Original dump file not found: {original_path}")
+        exit(1)
+
+
+if __name__ == "__main__":
+  main()

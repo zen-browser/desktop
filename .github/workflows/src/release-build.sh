7f298009824748136bfc56a9a62655e0ecd97658
mr. m <mr.m@tuta.com>
2026-01-07 13:42:42 +0100

    feat: Start implementing PGO profiling on different steps, b=no-bug, c=workflows, configs

--- .github/workflows/src/release-build.sh
+++ .github/workflows/src/release-build.sh
@@ -28,3 +28,22 @@ else
   export ZEN_RELEASE=1
   npm run build
 fi
+
+if test "$ZEN_GENERATE_PGO_DATA" = "1"; then
+  cd engine
+  export UPLOAD_PATH=../zen-macos-pgo-data
+  export MOZ_FETCHES_DIR=/Users/runner/.mozbuild
+  mkdir -p $UPLOAD_PATH
+
+  export JARLOG_FILE="en-US.log"
+
+  export LLVM_PROFDATA=$MOZ_FETCHES_DIR/clang/bin/llvm-profdata
+
+  set -v
+
+  ./mach python build/pgo/profileserver.py --binary obj-*-apple-darwin/dist/*.app/Contents/MacOS/zen
+
+  mv merged.profdata $UPLOAD_PATH/
+  mv $JARLOG_FILE $UPLOAD_PATH/
+  cd ..
+fi

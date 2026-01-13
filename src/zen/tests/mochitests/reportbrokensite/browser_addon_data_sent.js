04dbbf7d257c1def3210aac86a6175f28d286a97
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2026-01-13 20:55:42 +0100

    Discard changes to src/zen/tests/mochitests/reportbrokensite/browser_addon_data_sent.js

--- src/zen/tests/mochitests/reportbrokensite/browser_addon_data_sent.js
+++ src/zen/tests/mochitests/reportbrokensite/browser_addon_data_sent.js
@@ -90,7 +90,7 @@ add_task(async function testSendingMoreInfo() {
 
   const tab = await openTab(REPORTABLE_PAGE_URL);
 
-  await testSendMoreInfo(tab, AppMenu(), {
+  await testSendMoreInfo(tab, HelpMenu(), {
     addons: EXPECTED_ADDONS,
   });
 

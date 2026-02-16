ae82d74174eb71005d08c1cb08d0871413fa597e
Ivaon <155267770+Ivaon@users.noreply.github.com>
2026-02-16 10:22:40 +0100

    chore: fix implementation in wrong function

--- src/zen/folders/ZenFolder.mjs
+++ src/zen/folders/ZenFolder.mjs
@@ -247,7 +247,6 @@ export class nsZenFolder extends MozTabbrowserTabGroup {
 
   unloadAllTabs(event) {
     this.#unloadAllActiveTabs(event, /* noClose */ true);
-    this.collapsed = true;
   }
 
   async #unloadAllActiveTabs(event, noClose = false) {
@@ -257,6 +256,7 @@ export class nsZenFolder extends MozTabbrowserTabGroup {
       folderToUnload: this,
     });
     this.activeTabs = [];
+    this.collapsed = true;
   }
 
   on_click(event) {

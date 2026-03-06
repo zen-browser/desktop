ea0b8209fe645fe97f08023abc0228c8069486bd
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2026-03-06 12:12:36 +0100

    Discard changes to src/zen/mods/ZenStyleSheetCache.h

--- src/zen/mods/ZenStyleSheetCache.h
+++ src/zen/mods/ZenStyleSheetCache.h
@@ -6,57 +6,54 @@
 #define mozilla_ZenStyleSheetCache_h__
 
 #include "mozilla/css/Loader.h"
+#include "mozilla/NotNull.h"
 #include "mozilla/StaticPtr.h"
 
 #ifndef ZEN_MODS_FILENAME
-#  define ZEN_MODS_FILENAME
-e n - themes.css "_ns
+  #define ZEN_MODS_FILENAME u"zen-themes.css"_ns
 #endif
 
-      namespace zen {
+namespace zen {
 
-  class ZenStyleSheetCache final : public nsISupports {
-    using StyleSheet = mozilla::StyleSheet;
+class ZenStyleSheetCache final : public nsISupports {
+  using StyleSheet = mozilla::StyleSheet;
 
-   public:
-    NS_DECL_ISUPPORTS
+ public:
+  NS_DECL_ISUPPORTS
 
-    /**
-     * @brief Get the mods stylesheet.
-     * This is called when we need to get the mods stylesheets.
-     * @returns The mods stylesheet.
-     */
-    auto GetModsSheet() -> StyleSheet*;
+  /**
+   * @brief Get the mods stylesheet.
+   * This is called when we need to get the mods stylesheets.
+   * @returns The mods stylesheet.
+   */
+  auto GetModsSheet() -> StyleSheet*;
 
-    /**
-     * @brief Rebuild the mods stylesheets.
-     * This is re-parses the mods stylesheet and applies it to all
-     * the connected documents.
-     * @param aContents The contents of the mods stylesheet.
-     * @returns NS_OK on success, or an error code on failure.
-     */
-    nsresult RebuildModsStylesheets(const nsACString& aContents);
+  /**
+   * @brief Rebuild the mods stylesheets.
+   * This is re-parses the mods stylesheet and applies it to all
+   * the connected documents.
+   * @param aContents The contents of the mods stylesheet.
+   * @returns NS_OK on success, or an error code on failure.
+   */
+  nsresult RebuildModsStylesheets(const nsACString& aContents);
 
-    static auto Singleton() -> ZenStyleSheetCache*;
+  static auto Singleton() -> ZenStyleSheetCache*;
+ private:
+  ZenStyleSheetCache() = default;
+  ~ZenStyleSheetCache() = default;
 
-   private:
-    ZenStyleSheetCac
+  /**
+   * @brief Load the stylesheet from the given file.
+   * @param aFile The file to load the stylesheet from.
+   */
+  auto LoadSheetFile(nsIFile* aFile, mozilla::css::SheetParsingMode aParsingMode)
+      -> void;
 
-        () = default;
-    ~ZenStyleSheetCache() = default;
+  static mozilla::StaticRefPtr<ZenStyleSheetCache> gZenModsCache;
 
-    /**
-     * @brief Load the stylesheet from the given file.
-     * @param aFile The file to load the stylesheet from.
-     */
-    auto LoadSheetFile(nsIFile* aFile,
-                       mozilla::css::SheetParsingMo e aParsingMode) -> void;
+  RefPtr<StyleSheet> mModsSheet;
+};
 
-    static moz taticRefPtr<ZenStyleSheetCache> gZenModsCache;
-
-    RefPtr<StyleSheet> mModsSheet;
-  };
-
-}  // namespace zen
+} // namespace zen
 
 #endif

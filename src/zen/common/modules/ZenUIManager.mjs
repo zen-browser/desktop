f2b459da92efd45eeb9b026544ed8af5be51a445
Thomas <150849104+Lecloow@users.noreply.github.com>
2026-03-11 18:38:11 +0100

    fix: remove unnecessary double false arguments in handleUrlbarClose
    Signed-off-by: Thomas <150849104+Lecloow@users.noreply.github.com>

--- src/zen/common/modules/ZenUIManager.mjs
+++ src/zen/common/modules/ZenUIManager.mjs
@@ -440,7 +440,7 @@ window.gZenUIManager = {
 
     // Close the new tab popup on cmd/ctrl + t
     if (gURLBar.hasAttribute("zen-newtab")) {
-      this.handleUrlbarClose(false, false);
+      this.handleUrlbarClose();
       return true;
     }
 

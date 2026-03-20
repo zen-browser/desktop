44a1e330df490c5e84c210961d65746054d371f3
mr. m <mr.m@tuta.com>
2026-03-20 14:06:41 +0100

    no-bug: Update rust deps and VS version

--- build/windows/bootstrap.ps1
+++ build/windows/bootstrap.ps1
@@ -9,5 +9,5 @@ rustup target add aarch64-pc-windows-msvc
 rustup target add x86_64-pc-windows-msvc
 
 cd engine
-./mach python --virtualenv build taskcluster/scripts/misc/get_vs.py build/vs/vs2022.yaml ../win-cross/vs2022
+./mach python --virtualenv build taskcluster/scripts/misc/get_vs.py build/vs/vs2026.yaml ../win-cross/vs2026
 cd ..

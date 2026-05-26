06c1ec044401442d801f34a064d6e5b62ff004af
mr. m <mr.m@tuta.com>
2026-05-26 11:53:08 +0200

    no-bug: Change to overflow Y

--- src/zen/folders/ZenFolders.mjs
+++ src/zen/folders/ZenFolders.mjs
@@ -1566,6 +1566,7 @@ class nsZenFolders extends nsZenDOMOperatedFeature {
 
     const tabsContainer = group.groupContainer;
     tabsContainer.removeAttribute("hidden");
+    tabsContainer.style.overflowY = "hidden";
 
     const groupStart = group.groupStartElement;
     const itemsToShow = this.#normalizeGroupItems(group.childGroupsAndTabs);
@@ -1614,7 +1615,7 @@ class nsZenFolders extends nsZenDOMOperatedFeature {
     }
 
     const afterMarginTop = () => {
-      tabsContainer.style.overflow = "";
+      tabsContainer.style.overflowY = "";
       if (group.hasAttribute("has-active")) {
         const activeTabs = group.activeTabs;
         const folders = new Map();
@@ -1858,14 +1859,14 @@ class nsZenFolders extends nsZenDOMOperatedFeature {
 
             const tabsContainer = currentGroup.groupContainer;
             const groupStart = currentGroup.groupStartElement;
-            tabsContainer.style.overflow = "clip";
+            tabsContainer.style.overflowY = "clip";
 
             if (tabsContainer.hasAttribute("hidden")) {
               tabsContainer.removeAttribute("hidden");
             }
 
             const afterMarginTop = () => {
-              tabsContainer.style.overflow = "";
+              tabsContainer.style.overflowY = "";
             };
 
             animations.push(
@@ -1971,6 +1972,7 @@ class nsZenFolders extends nsZenDOMOperatedFeature {
     const heightContainer = expand
       ? 0
       : this.#calculateHeightShift(tabsContainer, []);
+    tabsContainer.style.overflowY = "clip";
 
     this.#createAnimation(
       groupStart,

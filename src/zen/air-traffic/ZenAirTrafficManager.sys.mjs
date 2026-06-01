9d6325b36b348aafbc2883b5ab36539336bd5981
fen4flo <fb8599422@gmail.com>
2026-06-01 22:57:50 +0200

    chore: Change to using system borders (broken)

--- src/zen/air-traffic/ZenAirTrafficManager.sys.mjs
+++ src/zen/air-traffic/ZenAirTrafficManager.sys.mjs
@@ -105,7 +105,7 @@ class nsZenAirTrafficManager {
     const control = parentWindow.openDialog(
       "chrome://browser/content/zen-components/windows/zen-air-traffic.xhtml",
       "",
-      "centerscreen,modal,dependent,resizable=no,dialog=yes,chrome,titlebar=no",
+      "centerscreen,modal,dependent,resizable=no,chrome,titlebar=no",
       { parentWindow },
     );
 

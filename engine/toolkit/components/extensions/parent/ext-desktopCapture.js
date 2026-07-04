c6343feb9b07a324d68a1a564342942e4a8ffcbc
devflex-ai <mahirusus@gmail.com>
2026-07-04 08:33:36 -0700

    add: ext-desktopCapture.js

---
+++ engine/toolkit/components/extensions/parent/ext-desktopCapture.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+this.desktopCapture = class extends ExtensionAPI {
+  getAPI(context) {
+    return {
+      desktopCapture: {
+        async chooseDesktopMedia(sources, targetTab) {
+          // Stub: return null stream ID
+          return null;
+        },
+
+        cancelChooseDesktopMedia(desktopMediaRequestId) {
+          // No-op stub
+        },
+      },
+    };
+  }
+};

d02979b4fd74d4de3749040f87a479d421044b9e
mr. m <mr.m@tuta.com>
2026-07-18 19:22:16 +0200

    no-bug: Fix lint

--- src/zen/folders/ZenFolders.mjs
+++ src/zen/folders/ZenFolders.mjs
@@ -1297,9 +1297,11 @@ class nsZenFolders extends nsZenDOMOperatedFeature {
             }
             default: {
               // Should insert after zen-empty-tab
-              const start =
+              let start =
                 parentWorkingData.node.groupStartElement.nextElementSibling;
-              start ||= parentWorkingData.node.tabs[0] || parentWorkingData.node.groupStartElement;
+              start ||=
+                parentWorkingData.node.tabs[0] ||
+                parentWorkingData.node.groupStartElement;
               if (!start) {
                 console.error(
                   `Zen Folders: Could not find start element for parent folder with id ${stateData.parentId} while restoring session.`

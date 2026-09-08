8f50c29845e11c689299f581529eb4545734e621
mr. m <mr.m@tuta.com>
2026-09-08 20:13:37 +0200

    Add context share functionality for split

--- src/zen/common/zen-sets.js
+++ src/zen/common/zen-sets.js
@@ -41,6 +41,9 @@ document.addEventListener(
           case "cmd_zenSplitViewContextMenu":
             gZenViewSplitter.contextSplitTabs();
             break;
+          case "cmd_zenCtxShareSplitView":
+            gZenViewSplitter.contextShareSplitView();
+            break;
           case "cmd_zenCopyCurrentURLMarkdown":
             gZenCommonActions.copyCurrentURLAsMarkdownToClipboard();
             break;

e780144d2a004daa27914baf9e7c20a17998151d
octaviusz <octaviusz@duck.com>
2026-09-11 00:07:09 +0300

    fix: bad gh merge

--- src/zen/glance/actors/ZenGlanceChild.sys.mjs
+++ src/zen/glance/actors/ZenGlanceChild.sys.mjs
@@ -17,13 +17,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
   true
 );
 
-XPCOMUtils.defineLazyPreferenceGetter(
-  lazy,
-  "activationMethod",
-  "zen.glance.activation-method",
-  "ctrl"
-);
-
 // A small threshold to allow for minor mouse jitter during a normal click.
 // Anything beyond this is likely an intentional drag (like selecting text).
 const CLICK_DRAG_THRESHOLD_PX = 4;
@@ -275,5 +268,9 @@ export class ZenGlanceChild extends JSWindowActorChild {
         this.contentWindow.document.activeElement !==
         this.contentWindow.document.body,
     });
+  }
+
+  async on_DOMContentLoaded() {
+    await this.#initActivationMethod();
   }
 }

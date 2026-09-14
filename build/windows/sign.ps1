b9ccf698605b808135f8ec74d1c6d41a6079e7f6
mr. m <mr.m@tuta.com>
2026-09-14 02:32:48 +0200

    no-bug: Fix versions

--- build/windows/sign.ps1
+++ build/windows/sign.ps1
@@ -147,9 +147,9 @@ function SignAndPackage($name) {
     $env:ZEN_SETUP_EXE_PATH="$PWD\windsign-temp\windows-x64-obj-$name\browser\installer\windows\instgen\setup.exe"
 
     if ($name -eq "arm64") {
-        $env:WIN32_REDIST_DIR="$PWD\win-cross\vs2026\VC\Redist\MSVC\14.51.36247\arm64\Microsoft.VC145.CRT"
+        $env:WIN32_REDIST_DIR="$PWD\win-cross\vs2026\VC\Redist\MSVC\14.51.36231\arm64\Microsoft.VC145.CRT"
     } else {
-        $env:WIN32_REDIST_DIR="$PWD\win-cross\vs2026\VC\Redist\MSVC\14.51.36247\x64\Microsoft.VC145.CRT"
+        $env:WIN32_REDIST_DIR="$PWD\win-cross\vs2026\VC\Redist\MSVC\14.51.36231\x64\Microsoft.VC145.CRT"
     }
 
     $env:MAR="..\\build\\windows\\mar.exe"

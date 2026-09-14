74ec2ac8bec8a504258c0f592c84ff3e8ebdd632
mr. m <mr.m@tuta.com>
2026-09-14 09:40:23 +0200

    no-bug: Fix session store import

--- src/zen/urlbar/ZenUBGlobalActions.sys.mjs
+++ src/zen/urlbar/ZenUBGlobalActions.sys.mjs
@@ -18,7 +18,8 @@ ChromeUtils.defineLazyGetter(lazy, "l10n", () => {
 });
 
 ChromeUtils.defineESModuleGetters(lazy, {
-  SessionStore: "resource:///modules/sessionstore/SessionStore.sys.mjs",
+  SessionStore:
+    "moz-src:///browser/components/sessionstore/SessionStore.sys.mjs",
 });
 
 function isNotEmptyTab(window) {

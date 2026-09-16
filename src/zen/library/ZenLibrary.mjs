d51cd439949bdb06c9748d33e9d24eb092356a3d
fen4flo <fb8599422@gmail.com>
2026-09-16 23:11:22 +0200

    no-bug: Fix web wrapper misalignment

--- src/zen/library/ZenLibrary.mjs
+++ src/zen/library/ZenLibrary.mjs
@@ -113,10 +113,10 @@ export class ZenLibrary extends MozLitElement {
   set openProgress(value) {
     const p = value;
     const stealWindowButtonsPastPoint = 0.6;
-    const wasOpen = this.#progress > 0;
+    const wasOpen = this.#progress > 0.001;
     this.#progress = p;
     const isPastWindowButtonSwitchPoint = p > stealWindowButtonsPastPoint;
-    const isOpen = p > 0;
+    const isOpen = p > 0.001;
 
     let libraryWidth = window.windowUtils.getBoundsWithoutFlushing(this).width;
     const compactModeOffsetDirection = this.#libraryOnRight

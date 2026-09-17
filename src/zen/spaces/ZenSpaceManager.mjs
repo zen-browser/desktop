b7b43a19c54e19d775f323e47c20673bbc5efd3c
octaviusz <octaviusz@duck.com>
2026-09-17 19:32:47 +0300

    fix-15293: Fix tab index calculation for CWLO using unclipped session data

--- src/zen/spaces/ZenSpaceManager.mjs
+++ src/zen/spaces/ZenSpaceManager.mjs
@@ -757,6 +757,9 @@ class nsZenWorkspaces {
       : [this.#createWorkspaceData("Space", undefined)];
     this.activeWorkspace =
       aWinData.activeZenSpace || this._workspaceCache[0].uuid;
+    if (aWinData.selected) {
+      this._sessionSelected = aWinData.selected;
+    }
     let promise = this.#initializeWorkspaces();
     for (const workspace of spacesFromStore) {
       const element = this.workspaceElement(workspace.uuid);
@@ -834,6 +837,7 @@ class nsZenWorkspaces {
     });
 
     const cleanup = () => {
+      delete this._sessionSelected;
       delete this._tabToSelect;
       delete this._tabToRemoveForEmpty;
       delete this._shouldOverrideTabs;
@@ -861,7 +865,10 @@ class nsZenWorkspaces {
       !removedEmptyTab &&
       !this._shouldOverrideTabs
     ) {
-      const tabs = gBrowser.tabs.filter(tab => !tab.collapsed);
+      const tabs = gBrowser.tabs;
+      if (Services.prefs.getBoolPref("zen.workspaces.continue-where-left-off")) {
+        this._tabToSelect = this._sessionSelected - 1;
+      }
       if (
         typeof this._tabToSelect === "number" &&
         this._tabToSelect >= 0 &&

b3467c0ff2de1c0342895bfcd1b2240bf65a84a1
mr. m <91018726+mr-cheffy@users.noreply.github.com>
2026-09-21 01:43:48 +0200

    gh-15494: Fixed library media tab freezing (gh-15495)

--- src/zen/common/zenThemeModifier.js
+++ src/zen/common/zenThemeModifier.js
@@ -110,7 +110,7 @@
           const targetRadius = window.matchMedia("(-moz-mac-tahoe-theme)")
             .matches
             ? 11
-            : 10;
+            : 9;
           document.documentElement.style.setProperty(
             "--zen-border-radius",
             targetRadius + "px"
